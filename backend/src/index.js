11faa46243f75c27045a50d37ac53d50ae737b2a
muhammadali <muhammadali@gmail.com>
2026-03-26 02:58:44 +0500

    test: trigger pipeline

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,3 +28,4 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error('MongoDB Error:', err));
+# test
